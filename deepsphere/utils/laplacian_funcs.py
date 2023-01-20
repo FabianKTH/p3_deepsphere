98dd20249271cb716c051876f9cfb74032aa25f7
FabianKTH <31001953+FabianKTH@users.noreply.github.com>
2023-01-20 16:50:33 +0100

    [INIT] sime hotfixes to get some of the code to run

--- deepsphere/utils/laplacian_funcs.py
+++ deepsphere/utils/laplacian_funcs.py
@@ -79,7 +79,7 @@ def get_icosahedron_laplacians(nodes, depth, laplacian_type):
     for _ in range(depth):
         nodes = icosahedron_nodes_calculator(order)
         order_initial = icosahedron_order_calculator(nodes)
-        G = SphereIcosahedron(level=int(order_initial))
+        G = SphereIcosahedron(subdivisions=2**int(order_initial))
         G.compute_laplacian(laplacian_type)
         laplacian = prepare_laplacian(G.L)
         laps.append(laplacian)
